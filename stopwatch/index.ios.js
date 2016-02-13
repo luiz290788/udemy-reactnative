2b06a93a807685930e70091ee7f82e661e98e6a2
Luiz Guilherme D'Abruzzo Pereira <luiz290788@gmail.com>
2016-02-13 16:40:09 -0200

    code after lecture 21

---
+++ stopwatch/index.ios.js
@@ -0,0 +1,18 @@
+'use strict';
+
+import React, {
+  Text,
+  View,
+  Component,
+  AppRegistry
+} from 'react-native';
+
+class StopWatch extends Component {
+  render() {
+    return (
+
+    );
+  }
+}
+
+AppRegistry.registerComponent('stopwatch', () => StopWatch);
