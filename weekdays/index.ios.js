caab6f6ae3aab5aba6c71e8e0d5b64dd16bfdcec
Luiz Guilherme D'Abruzzo Pereira <luiz290788@gmail.com>
2016-02-13 15:09:38 -0200

    code after lecture 11

--- weekdays/index.ios.js
+++ weekdays/index.ios.js
@@ -4,15 +4,32 @@ import React, {
   AppRegistry,
   Component,
   View,
-  Text
+  Text,
+  StyleSheet
 } from 'react-native';
 
 class Weekdays extends Component {
   render() {
-    return <View>
-    <Text>Days of the week:</Text>
-    </View>
+    return (
+      <View style={styles.container}>
+        <Text>Days of the week:</Text>
+      </View>
+    )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+
+    // inverts justifyContent and alignItems
+    // flexDirection: 'row',
+
+    // controls vertically
+    justifyContent: 'center',
+    // controls horizontally
+    alignItems: 'center'
+  }
+});
 
 AppRegistry.registerComponent('weekdays', () => Weekdays);
