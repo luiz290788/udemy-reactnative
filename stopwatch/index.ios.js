9d2719ab6bda5a5b2121bb27ddd368c6a2053c13
Luiz Guilherme D'Abruzzo Pereira <luiz290788@gmail.com>
2016-02-13 17:02:20 -0200

    code after lecture 23

--- stopwatch/index.ios.js
+++ stopwatch/index.ios.js
@@ -3,17 +3,26 @@
 import React, {
   Text,
   View,
-  Component,
+  StyleSheet,
   AppRegistry
 } from 'react-native';
 
 const StopWatch = React.createClass({
   render: function() {
     return (
-      <View>
-        <Text>00:00:00</Text>
-        {this.startStopButton()}
-        {this.lapButton()}
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <View>
+            <Text>00:00:00</Text>
+          </View>
+          <View>
+            {this.startStopButton()}
+            {this.lapButton()}
+          </View>
+        </View>
+        <View style={styles.footer}>
+          <Text>I am a list of laps</Text>
+        </View>
       </View>
     );
   },
@@ -30,6 +39,27 @@ const StopWatch = React.createClass({
         <Text>Lap</Text>
       </View>
     );
+  }
+});
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, // fill the entire screen
+    alignItems: 'stretch'
+  },
+  header: {
+    flex: 1,
+    backgroundColor: 'yellow'
+  },
+  footer: {
+    flex: 1,
+    backgroundColor: 'blue'
+  },
+  timer: {
+    backgroundColor: 'red'
+  },
+  buttons: {
+    backgroundColor: 'green'
   }
 });
 
