8c13ba2de80699df6174179b112461cd2cd26541
Luiz Guilherme D'Abruzzo Pereira <luiz290788@gmail.com>
2016-02-15 21:24:11 -0200

    code after lecture 42

--- weather/src/api.js
+++ weather/src/api.js
@@ -1,12 +1,18 @@
 'use strict';
 
-import React, {
-  fetch
-} from 'react-native';
-
 const apiUrl = 'http://api.openweathermap.org/data/2.5/weather?APPID=f1c9da0fce63c1c758fa825a2cfeace0';
+
+function kelvinToC(kelvin) {
+  return (kelvin - 273.15) + '˚C';
+}
 
 export default function openWeather (latitude, longitude) {
   var url = `${apiUrl}&lat=${latitude}&lon=${longitude}`;
-  
+  fetch(url).then(response => response.json()).then(json => {
+    return {
+      city: json.name,
+      temperature: kelvinToC(json.main.temp),
+      description: json.weather[0].description
+    }
+  });
 }
